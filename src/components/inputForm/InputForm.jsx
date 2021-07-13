bed83cfca84f2b344dfc8dc9d11a14ff5ffd8b3e
ErikGraciosa <erikgraciosa@gmail.com>
2021-07-12 17:15:38 -0700

    basic form and image

---
+++ src/components/inputForm/InputForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function inputForm() {
+  return (
+    <div>
+      <form>
+        <input type='text'></input>
+        <button>Generate Number</button>
+      </form>
+    </div>
+  )
+}
+
+export default inputForm
